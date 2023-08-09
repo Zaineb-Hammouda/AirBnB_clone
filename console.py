8af17c6c57b0099cf72e3bb0e7b78c741b8e4e6d
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2023-08-09 17:11:54 +0200

    task 8- console 0.1 version, added update(), all() and destroy()

--- console.py
+++ console.py
@@ -29,6 +29,7 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def emptyline(self):
+        """an empty line + ENTER shouldn’t execute anything"""
         pass
 
     def do_create(self, line):
@@ -39,7 +40,10 @@ class HBNBCommand(cmd.Cmd):
 
         if (self.errors(line, "create") == 1):
             return False
-        instance = eval(line)
+        """ evaluate the contents of line string to class and execute it
+        with ()"""
+        instance = eval(line)()
+        """save it to JSON file"""
         instance.save()
         print(instance.id)
 
@@ -51,51 +55,88 @@ class HBNBCommand(cmd.Cmd):
 
         if (self.errors(line, "show") == 1):
             return False
+        """split line to multiple args"""
         args = line.split()
+        """get all instances's dicts from storage"""
         instances_dict = storage.all()
+        """remove the quotations if string inputted has them"""
         for i in range(len(args)):
             if args[i][0] == '"':
                 args[i] = args[i].replace('"', "")
         print(instances_dict[args[0] + '.' + args[1]])
 
-    def do_destroy():
+    def do_destroy(self, line):
         """Delete command: deletes an instance based on the class name and id
         Ex: $ destroy BaseModel 1234-1234-1234
         """
         if (self.errors(line, "destroy") == 1):
             return False
 
-    def do_update():
+        instances_dict = storage.all()
+        args = line.split()
+        for i in range(len(args)):
+            if args[i][0] == '"':
+                args[i] = args[i].replace('"', "")
+        del instances_dict[args[0] + '.' + args[1]]
+        storage.save()
+
+    def do_update(self, line):
         """Updates an instance based on the class name and id
         Ex: $ update BaseModel 1234-1234-1234 email "aibnb@mail.com"
+        Usage: update <class name> <id> <attribute name> "<attribute value>"
         """
         if (self.errors(line, "update") == 1):
             return False
 
-    def do_all():
+        args = line.split()
+        instances_dict = storage.all()
+        for i in range(len(args)):
+            if args[i][0] == '"':
+                args[i] = args[i].replace('"', "")
+        x = args[0] + '.' + args[1]
+        new_key = args[2]
+        new_value = args[3]
+        setattr(instances_dict[x], new_key, new_value)
+        storage.save()
+
+    def do_all(self, line):
         """all command: prints all string representation of all
         instances based or not on the class name.
         Ex: $ all BaseModel or $ all
         """
+        instances_dict = storage.all()
+        """if all is used without class name"""
+        d_list = []
+        if line == "":
+            d_list = [str(i) for i in instances_dict.values()]
+            print(d_list)
+            return False
+        args = line.split()
+        """if all is used with class name, manage errors, if any, and return"""
         if (self.errors(line, "all") == 1):
             return False
+        for i in instances_dict.values():
+            if i.__class__.__name__ == args[0]:
+                d_list.append(str(i))
+        print(d_list)
 
     def errors(self, line, cmd):
         """manages error messages for user input"""
 
         cls_list = ["BaseModel", "FileStorage"]
+        cmd_list = ["create", "show", "all", "destroy", "update"]
 
-        args = line.split()
         if line == "":
             print("** class name missing **")
             return 1
-        if args[0] not in cls_list and cmd == "create":
+        args = line.split()
+        if args[0] not in cls_list and cmd in cmd_list:
             print("** class doesn't exist **")
             return 1
-        elif cmd == "create":
+        elif cmd in ["create", "all"]:
             return 0
 
-        if len(args) < 2 and cmd in ["show", "destroy"]:
+        if len(args) < 2 and cmd in ["show", "destroy", "update"]:
             print("** instance id missing **")
             return 1
 
@@ -105,7 +146,7 @@ class HBNBCommand(cmd.Cmd):
                 args[i] = args[i].replace('"', "")
         k = args[0] + '.' + args[1]
 
-        if k not in instances_dict and cmd in ["show", "destroy"]:
+        if k not in instances_dict and cmd in ["show", "destroy", "update"]:
             print("** no instance found **")
             return 1
         elif cmd in ["show", "destroy"]:
@@ -119,5 +160,6 @@ class HBNBCommand(cmd.Cmd):
             return 1
         return 0
 
+
 if __name__ == '__main__':
-        HBNBCommand().cmdloop()
+    HBNBCommand().cmdloop()
